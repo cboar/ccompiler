64b646a8c4d0b9f2a835210e49363c5c48cf63c6
Joel N <cboar@users.noreply.github.com>
2015-09-05 21:44:35 -0700

    Created Production struct

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,9 @@
+#include <stddef.h>
 #include "parser.h"
 #include "lexer.h"
 
 void build_productions()
 {
+	Production C[128], T[130];
 
 }
--- src/parser.h
+++ src/parser.h
@@ -1,5 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+typedef struct Production Production;
+struct Production {
+	Production* prods;
+	int data;
+};
+
+void build_productions();
 
 #endif /* PARSER_H */
